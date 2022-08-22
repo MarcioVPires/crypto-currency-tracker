49dbc08b32929f16007a5e8e0c0ce81bc9cc0622
Marcio <semEmail@gmafil.com>
2022-08-22 12:24:56 -0300

    removing search view

--- src/components/MainBarComp.vue
+++ src/components/MainBarComp.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="w-full flex flex-col justify-center items-center pt-14 sm:pt-16">
     <div class="w-full flex flex-col sm:flex-row sm:justify-between text-center pt-10 pb-4">
-      <!-- <SearchComp @search="$emit('search', $event)" /> -->
       <NavComp />
     </div>
   </section>
@@ -9,14 +8,12 @@
 
 <script>
 import NavComp from "@/components/NavComp.vue";
-// import SearchComp from "@/components/SearchComp.vue";
 
 export default {
   name: "WatchList",
   props: ["page"],
   components: {
     NavComp,
-    // SearchComp,
   },
 };
 </script>
